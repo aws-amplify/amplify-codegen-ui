50d408b112700d89e46bbf352a15a35b8d40dd8e
Jean Ro <frimfram+github@amazon.com>
2021-07-09 14:18:26 -0700

    Added Button that points to new props

---
+++ packages/studio-ui-codegen-react/lib/amplify-ui-renderers/button.ts
@@ -0,0 +1,30 @@
+import { ButtonProps } from "@aws-amplify/ui-react-types";
+
+import {
+  StudioComponent,
+  StudioComponentProperties,
+} from "@amzn/amplify-ui-codegen-schema";
+
+import { ReactComponentRenderer } from "../react-component-renderer"
+
+import ts, { factory, JsxElement } from "typescript";
+
+export default class ButtonRenderer extends ReactComponentRenderer<ButtonProps, ButtonProps> {
+  renderElement(): ts.JsxElement {
+    const tagName = "Button";
+
+    const element = factory.createJsxElement(
+      this.renderOpeningElement(factory, this.component.properties, tagName),
+      [],
+      factory.createJsxClosingElement(factory.createIdentifier(tagName))
+    );
+
+    this.importCollection.addImport("@amzn-amplify/amplify-ui", tagName);
+    return element;
+  }
+
+  mapProps(props: ButtonProps): ButtonProps {
+    return {
+    };
+  }
+}
