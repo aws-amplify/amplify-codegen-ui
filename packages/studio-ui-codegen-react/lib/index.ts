7704d00c4fa90becac6fe4888c53ddf7bef262c2
Zeyu Li <lizeyu@amazon.com>
2021-07-21 13:55:14 -0700

    fix export

--- packages/studio-ui-codegen-react/lib/index.ts
+++ packages/studio-ui-codegen-react/lib/index.ts
@@ -1,1 +1,5 @@
 export * from "./amplify-ui-renderers/amplify-renderer";
+export * from "./react-component-with-children-renderer";
+export * from "./react-component-renderer";
+export { ImportCollection } from "./import-collection";
+export * from "./react-studio-template-renderer";
