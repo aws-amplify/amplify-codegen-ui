f489b6824b4764fd917f150078bf93c471e4172a
Zeyu Li <lizeyu@amazon.com>
2021-07-22 16:10:19 -0700

    export output manager

--- packages/studio-ui-codegen-react/lib/index.ts
+++ packages/studio-ui-codegen-react/lib/index.ts
@@ -3,3 +3,4 @@ export * from "./react-component-with-children-renderer";
 export * from "./react-component-renderer";
 export { ImportCollection } from "./import-collection";
 export * from "./react-studio-template-renderer";
+export * from "./react-output-manager";
