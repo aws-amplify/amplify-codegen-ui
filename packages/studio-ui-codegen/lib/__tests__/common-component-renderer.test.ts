6583017120153838fde58432ebec62bfbdfdeeec
Dane Pilcher <dppilche@amazon.com>
2021-09-15 16:52:39 +0000

    test(studio-ui-codegen): add tests for CommonComponentRenderer

---
+++ packages/studio-ui-codegen/lib/__tests__/common-component-renderer.test.ts
@@ -0,0 +1,24 @@
+import { BaseComponentProps } from '@amzn/amplify-ui-react-types';
+import { StudioNode } from '../studio-node';
+import { CommonComponentRenderer } from '../common-component-renderer';
+
+class MockComponentRenderer extends CommonComponentRenderer<BaseComponentProps> {}
+
+describe('common-component-renderer', () => {
+  test('constructor', () => {
+    const component = {
+      componentType: 'Button',
+      name: 'MyButton',
+      properties: {
+        value: { value: 'Confirm' },
+      },
+    };
+    const parent = new StudioNode({
+      componentType: 'View',
+      name: 'MyView',
+      properties: {},
+    });
+    const renderer = new MockComponentRenderer(component, parent);
+    expect(renderer).toMatchSnapshot();
+  });
+});
