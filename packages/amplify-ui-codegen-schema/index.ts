1bfd4287acf7b2d5f410f045e17658929cb60eb3
Jean Ro <606238+frimfram@users.noreply.github.com>
2021-09-29 15:47:46 -0700

    feat: concatenation binding implementation (#99)

--- packages/amplify-ui-codegen-schema/index.ts
+++ packages/amplify-ui-codegen-schema/index.ts
@@ -1,8 +1,3 @@
 export * from './lib/types/index';
 export * from './lib/types/primitives';
 export * from './lib/types/studio-schema';
-
-/*  legacy
-export * from "./lib/types/studio-component-property-type";
-export * from "./lib/types/studio-component";
-*/
