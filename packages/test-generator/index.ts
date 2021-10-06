389ad43934c8629858380868857167dff3f4cd76
Alexander Harris <alharris@amazon.com>
2021-10-06 10:13:28 -0700

    fix: enable info level logging, convert test-generator output to markdown

--- packages/test-generator/index.ts
+++ packages/test-generator/index.ts
@@ -9,6 +9,8 @@ import * as schemas from './lib';
 
 Error.stackTraceLimit = Infinity;
 
+log.setLevel('info');
+
 const renderConfig: ReactRenderConfig = {
   module: ModuleKind.CommonJS,
   target: ScriptTarget.ES2015,
@@ -23,28 +25,33 @@ const outputPathDir = path.resolve(path.join(__dirname, '..', 'ui-components'));
 const outputConfig: ReactOutputConfig = {
   outputPathDir,
 };
+
+const decorateTypescriptWithMarkdown = (typescriptSource: string): string => {
+  return `\`\`\`typescript jsx\n${typescriptSource}\n\`\`\``;
+};
+
 const rendererManager = new StudioTemplateRendererManager(rendererFactory, outputConfig);
 Object.entries(schemas).forEach(([name, schema]) => {
-  log.info(name);
+  log.info(`# ${name}`);
   try {
     rendererManager.renderSchemaToTemplate(schema as any);
     const buildRenderer = rendererFactory.buildRenderer(schema as any);
 
     const compOnly = buildRenderer.renderComponentOnly();
-    log.info('Component Only Output');
-    log.info('componentImports ');
-    log.info(compOnly.importsText);
-    log.info('componentText ');
-    log.info(compOnly.compText);
+    log.info('## Component Only Output');
+    log.info('### componentImports');
+    log.info(decorateTypescriptWithMarkdown(compOnly.importsText));
+    log.info('### componentText');
+    log.info(decorateTypescriptWithMarkdown(compOnly.compText));
 
     const compOnlyAppSample = buildRenderer.renderSampleCodeSnippet();
-    log.info('Code Snippet Output');
-    log.info('componentImports ');
-    log.info(compOnlyAppSample.importsText);
-    log.info('componentText ');
-    log.info(compOnlyAppSample.compText);
+    log.info('## Code Snippet Output');
+    log.info('### componentImports');
+    log.info(decorateTypescriptWithMarkdown(compOnlyAppSample.importsText));
+    log.info('### componentText');
+    log.info(decorateTypescriptWithMarkdown(compOnlyAppSample.compText));
   } catch (err) {
-    log.info(`${name} failed with error:`);
-    log.info(err);
+    log.error(`${name} failed with error:`);
+    log.error(err);
   }
 });
