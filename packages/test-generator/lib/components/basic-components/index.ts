c3ee4e77451d542b394c2b119704950f9b28d148
Alexander Harris <alharris@amazon.com>
2021-10-19 11:23:20 -0700

    fix: adding tests for basic components, stubbing remaining basic components

---
+++ packages/test-generator/lib/components/basic-components/index.ts
@@ -0,0 +1,25 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+export { default as BasicComponentBadge } from './basicComponentBadge.json';
+export { default as BasicComponentBox } from './basicComponentBox.json';
+export { default as BasicComponentButton } from './basicComponentButton.json';
+export { default as BasicComponentCard } from './basicComponentCard.json';
+export { default as BasicComponentCollection } from './basicComponentCollection.json';
+export { default as BasicComponentCustom } from './basicComponentCustom.json';
+export { default as BasicComponentDivider } from './basicComponentDivider.json';
+export { default as BasicComponentFlex } from './basicComponentFlex.json';
+export { default as BasicComponentImage } from './basicComponentImage.json';
+export { default as BasicComponentText } from './basicComponentText.json';
--- packages/test-generator/test-app-templates/src/App.tsx
+++ packages/test-generator/test-app-templates/src/App.tsx
@@ -17,17 +17,29 @@ import React from 'react';
 import BoxTest from './ui-components/BoxTest';
 import BoxWithButton from './ui-components/BoxWithButton';
 import CustomButton from './ui-components/CustomButton';
-import CustomText from './ui-components/CustomText';
 import withTheme from './ui-components/theme';
+import BasicComponentBadge from './ui-components/BasicComponentBadge';
+import BasicComponentBox from './ui-components/BasicComponentBox';
+import BasicComponentButton from './ui-components/BasicComponentButton';
+import BasicComponentCard from './ui-components/BasicComponentCard';
+import BasicComponentText from './ui-components/BasicComponentText';
 /* eslint-enable import/extensions */
 
 function App() {
   return (
     <>
+      <h1>Generated Component Tests</h1>
+      <div id={'basic-components'}>
+        <h2>Basic Components</h2>
+        <BasicComponentBadge />
+        <BasicComponentBox />
+        <BasicComponentButton />
+        <BasicComponentCard />
+        <BasicComponentText />
+      </div>
       <BoxTest />
       <BoxWithButton />
       <CustomButton />
-      <CustomText />
       {/* <TextWithDataBinding /> // TODO: add back in with data binding tests /*}
       {/*
       TODO: buttonUser Listed as optional prop, but fails when not present
