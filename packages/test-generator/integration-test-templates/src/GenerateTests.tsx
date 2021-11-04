3c6beed31f7980422a6d2cbd5cdec4937ad6fed9
Alexander Harris <alharris@amazon.com>
2021-11-04 10:00:40 -0700

    fix: updating tests now that react router v6 is part of create-react-app

--- packages/test-generator/integration-test-templates/src/GenerateTests.tsx
+++ packages/test-generator/integration-test-templates/src/GenerateTests.tsx
@@ -13,7 +13,6 @@
   See the License for the specific language governing permissions and
   limitations under the License.
  */
-import React from 'react';
 import { BrowserTestGenerator } from '@amzn/test-generator';
 
 export default function GenerateTests() {
