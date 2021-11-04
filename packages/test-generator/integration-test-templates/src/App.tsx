6f048d68640886f6caf78cbd84dacef055fa5bc7
Alexander Harris <alharris@amazon.com>
2021-11-04 10:00:40 -0700

    fix: updating tests now that react router v6 is part of create-react-app

--- packages/test-generator/integration-test-templates/src/App.tsx
+++ packages/test-generator/integration-test-templates/src/App.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ComponentTests from './ComponentTests';
 import GenerateTests from './GenerateTests';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Codegen UI Functional Tests</h1>
+      <ul>
+        <li>
+          <a href="/component-tests">Component Tests</a>
+        </li>
+        <li>
+          <a href="/generate-tests">Browser Generation Tests</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <div>home</div>
-        </Route>
-        <Route path="/component-tests">
-          <ComponentTests />
-        </Route>
-        <Route path="/generate-tests">
-          <GenerateTests />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/component-tests" element={<ComponentTests />} />
+        <Route path="/generate-tests" element={<GenerateTests />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
