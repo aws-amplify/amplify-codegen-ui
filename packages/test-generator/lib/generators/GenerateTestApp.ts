46f65ccef5cc748d7c86025c81573c64ed4afa3d
Alexander Harris <alharris@amazon.com>
2021-11-06 00:18:59 -0700

    feat: test-generate each case individually, add support for error cases as well

--- packages/test-generator/lib/generators/GenerateTestApp.ts
+++ packages/test-generator/lib/generators/GenerateTestApp.ts
@@ -15,22 +15,27 @@
  */
 import { NodeTestGenerator } from './NodeTestGenerator';
 
-new NodeTestGenerator({
+const DISABLED_SCHEMAS = [
+  'ComponentWithActionSignOut', // TODO: Support Auth Action E2E Tests
+  'ComponentWithActionNavigation', // TODO: Support Navigation Action E2E Tests
+  'ComplexTest1', // TODO: Complex tests are broken
+  'ComplexTest2', // TODO: Complex tests are broken
+  'ComplexTest3', // TODO: Complex tests are broken
+  'ComplexTest4', // TODO: Complex tests are broken
+  'ComplexTest5', // TODO: Complex tests are broken
+  'ComplexTest6', // TODO: Complex tests are broken
+  'ComplexTest7', // TODO: Complex tests are broken
+  'ComplexTest8', // TODO: Complex tests are broken
+  'ComponentMissingProperties', // Expected failure cases
+  'ComponentMissingType', // Expected failure cases
+];
+
+const generator = new NodeTestGenerator({
   writeToLogger: false,
   writeToDisk: true,
-  disabledSchemas: [
-    'ComponentWithActionSignOut', // TODO: Support Auth Action E2E Tests
-    'ComponentWithActionNavigation', // TODO: Support Navigation Action E2E Tests
-    'ComplexTest1', // TODO: Complex tests are broken
-    'ComplexTest2', // TODO: Complex tests are broken
-    'ComplexTest3', // TODO: Complex tests are broken
-    'ComplexTest4', // TODO: Complex tests are broken
-    'ComplexTest5', // TODO: Complex tests are broken
-    'ComplexTest6', // TODO: Complex tests are broken
-    'ComplexTest7', // TODO: Complex tests are broken
-    'ComplexTest8', // TODO: Complex tests are broken
-  ],
   outputConfigOverride: {
     outputPathDir: 'packages/integration-test/src/ui-components',
   },
-}).generate();
+});
+
+generator.generate(generator.getTestCases(DISABLED_SCHEMAS));
