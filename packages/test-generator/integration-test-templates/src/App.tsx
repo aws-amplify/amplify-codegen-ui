c8944efe2b5c43ca86f0678859751991cce25b8f
Dane Pilcher <dppilche@amazon.com>
2021-11-08 17:13:45 +0000

    test: integ test component snippet generation in node

--- packages/test-generator/integration-test-templates/src/App.tsx
+++ packages/test-generator/integration-test-templates/src/App.tsx
@@ -18,6 +18,7 @@ import ComponentTests from './ComponentTests';
 import GenerateTests from './GenerateTests';
 import PrimitivesTests from './PrimitivesTests';
 import ComplexTests from './ComplexTests';
+import SnippetTests from './SnippetTests'; // eslint-disable-line import/extensions
 
 const HomePage = () => {
   return (
@@ -36,6 +37,9 @@ const HomePage = () => {
         <li>
           <a href="/complex-tests">Complex Tests</a>
         </li>
+        <li>
+          <a href="/snippet-tests">Snippet Tests</a>
+        </li>
       </ul>
     </div>
   );
@@ -49,6 +53,7 @@ export default function App() {
         <Route path="/generate-tests" element={<GenerateTests />} />
         <Route path="/primitives-tests" element={<PrimitivesTests />} />
         <Route path="/complex-tests" element={<ComplexTests />} />
+        <Route path="/snippet-tests" element={<SnippetTests />} />
         <Route path="*" element={<HomePage />} />
       </Routes>
     </Router>
