e2a553a659c2928d2f45ac2a65999754d9a45714
Dane Pilcher <dppilche@amazon.com>
2021-11-17 11:20:54 -0700

    style: enable eslint on integ templates (#231)

--- packages/test-generator/integration-test-templates/cypress/integration/generated-components-spec.js
+++ packages/test-generator/integration-test-templates/cypress/integration/generated-components-spec.js
@@ -1,3 +1,18 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 describe('Generated Components', () => {
   describe('Sanity Test', () => {
     it('Successfully opens the app', () => {
@@ -359,14 +374,12 @@ describe('Generated Components', () => {
   describe('Icons', () => {
     it('Renders IconCloud', () => {
       cy.visit('http://localhost:3000/component-tests');
-      cy.get('#icons')
-        .find('svg')
-        .find('path')
-        .should(
-          'have.attr',
-          'd',
-          'M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96z',
-        );
+      cy.get('#icons').find('svg').find('path').should(
+        'have.attr',
+        'd',
+        // eslint-disable-next-line max-len
+        'M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96z',
+      );
     });
   });
 });
