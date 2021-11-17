03b8ab40bfd7068b6caee9525ab4caf1b3eff8ed
Dane Pilcher <dppilche@amazon.com>
2021-11-17 11:20:54 -0700

    style: enable eslint on integ templates (#231)

--- packages/test-generator/integration-test-templates/cypress/integration/primitives-spec.js
+++ packages/test-generator/integration-test-templates/cypress/integration/primitives-spec.js
@@ -1,3 +1,18 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 describe('Primitives', () => {
   describe('Sanity Test', () => {
     it('Successfully opens the app', () => {
@@ -115,6 +130,7 @@ describe('Primitives', () => {
           cy.get('path').should(
             'have.attr',
             'd',
+            // eslint-disable-next-line max-len
             'M11 7H13V9H11V7ZM11 11H13V17H11V11ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z',
           );
         });
--- packages/test-generator/integration-test-templates/src/models/index.d.ts
+++ packages/test-generator/integration-test-templates/src/models/index.d.ts
@@ -1,7 +1,20 @@
-/**
- * Note: This file was generated using Amplify Sandbox to reflect a realistic model.
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
-import { ModelInit, MutableModel, PersistentModelConstructor } from '@aws-amplify/datastore';
+/* eslint-disable max-classes-per-file */
+import { ModelInit, MutableModel } from '@aws-amplify/datastore';
 
 type UserPreferenceMetaData = {
   readOnlyFields: 'createdAt' | 'updatedAt';
@@ -17,8 +30,11 @@ type ListingMetaData = {
 
 export declare class UserPreference {
   readonly id: string;
+
   readonly favoriteColor?: string;
+
   readonly createdAt?: string;
+
   readonly updatedAt?: string;
   constructor(init: ModelInit<UserPreference, UserPreferenceMetaData>);
   static copyOf(
@@ -31,13 +47,21 @@ export declare class UserPreference {
 
 export declare class User {
   readonly id: string;
+
   readonly firstName?: string;
+
   readonly lastName?: string;
+
   readonly age?: number;
+
   readonly isLoggedIn?: boolean;
+
   readonly loggedInColor?: string;
+
   readonly loggedOutColor?: string;
+
   readonly createdAt?: string;
+
   readonly updatedAt?: string;
   constructor(init: ModelInit<User, UserMetaData>);
   static copyOf(
@@ -48,10 +72,15 @@ export declare class User {
 
 export declare class Listing {
   readonly id: string;
+
   readonly title?: string;
+
   readonly priceUSD?: number;
+
   readonly description?: string;
+
   readonly createdAt?: string;
+
   readonly updatedAt?: string;
   constructor(init: ModelInit<Listing, ListingMetaData>);
   static copyOf(
