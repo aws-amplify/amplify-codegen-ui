a5f1547f86cca16232e384ac8d783e237afa3fde
Dane Pilcher <dppilche@amazon.com>
2021-11-29 15:59:13 +0000

    ci: allow release as commit scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
+const {
+  utils: { getPackages },
+} = require('@commitlint/config-lerna-scopes');
+
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
+  rules: {
+    'scope-enum': async (context) => [2, 'always', [...(await getPackages(context)), 'release']],
+  },
 };
