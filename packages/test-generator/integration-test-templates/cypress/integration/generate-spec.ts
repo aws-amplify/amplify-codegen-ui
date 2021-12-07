3dcc23091bffd067cc286e0be27a81ae76c6bb39
Alexander Harris <ademp90@gmail.com>
2021-12-06 22:43:15 -0800

    chore: break out generate tests into their own test per case

--- packages/test-generator/integration-test-templates/cypress/integration/generate-spec.ts
+++ packages/test-generator/integration-test-templates/cypress/integration/generate-spec.ts
@@ -13,6 +13,98 @@
   See the License for the specific language governing permissions and
   limitations under the License.
  */
+const EXPECTED_SUCCESSFUL_CASES = new Set([
+  'ViewWithButton',
+  'ViewTest',
+  'CustomButton',
+  'ComponentWithActionSignOut',
+  'ComponentWithActionNavigation',
+  'ParsedFixedValues',
+  'MyIconCloud',
+  'Icons',
+  'BasicComponentBadge',
+  'BasicComponentView',
+  'BasicComponentButton',
+  'BasicComponentCard',
+  'BasicComponentCollection',
+  'BasicComponentDivider',
+  'BasicComponentFlex',
+  'BasicComponentImage',
+  'BasicComponentText',
+  'BasicComponentCustomRating',
+  'ComponentWithDataBindingWithPredicate',
+  'ComponentWithDataBindingWithoutPredicate',
+  'ComponentWithSimplePropertyBinding',
+  'ComponentWithAuthBinding',
+  'CompWithMultipleBindingsWithPred',
+  'BoundDefaultValue',
+  'SimplePropertyBindingDefaultValue',
+  'SimpleAndBouldDefaultValue',
+  'CollectionDefaultValue',
+  'ComplexTest1',
+  'ComplexTest2',
+  'ComplexTest3',
+  'ComplexTest4',
+  'ComplexTest5',
+  'ComplexTest6',
+  'ComplexTest7',
+  'ComplexTest8',
+  'ReneButton',
+  'CollectionWithBinding',
+  'CollectionWithSort',
+  'CollectionWithBindingItemsName',
+  'PaginatedCollection',
+  'CustomParent',
+  'CustomChildren',
+  'CustomParentAndChildren',
+  'ComponentWithConcatenation',
+  'ComponentWithConditional',
+  'ComponentWithBoundPropertyConditional',
+  'ComponentWithNestedOverrides',
+  'AlertPrimitive',
+  'BadgePrimitive',
+  'ButtonPrimitive',
+  'ButtonGroupPrimitive',
+  'CardPrimitive',
+  'CheckboxFieldPrimitive',
+  'CollectionPrimitive',
+  'DividerPrimitive',
+  'ExpanderPrimitive',
+  'FlexPrimitive',
+  'GridPrimitive',
+  'HeadingPrimitive',
+  'IconPrimitive',
+  'ImagePrimitive',
+  'LinkPrimitive',
+  'LoaderPrimitive',
+  'PaginationPrimitive',
+  'PasswordFieldPrimitive',
+  'PhoneNumberFieldPrimitive',
+  'PlaceholderPrimitive',
+  'RadioPrimitive',
+  'RadioGroupFieldPrimitive',
+  'RatingPrimitive',
+  'ScrollViewPrimitive',
+  'SearchFieldPrimitive',
+  'SelectFieldPrimitive',
+  'SliderFieldPrimitive',
+  'StepperFieldPrimitive',
+  'SwitchFieldPrimitive',
+  'TabsPrimitive',
+  'TextPrimitive',
+  'TextFieldPrimitive',
+  'ToggleButtonPrimitive',
+  'ToggleButtonGroupPrimitive',
+  'ViewPrimitive',
+  'VisuallyHiddenPrimitive',
+  'ComponentWithVariant',
+  'ComponentWithVariantAndOverrides',
+  'MyTheme',
+  'ViewWithButton',
+  'ViewTest',
+  'CustomButton',
+  'ComponentWithActionSignOut',
+]);
 const EXPECTED_INVALID_INPUT_CASES = new Set([
   'ComponentMissingProperties',
   'ComponentMissingType',
@@ -28,21 +120,59 @@ describe('Generate Components', () => {
     cy.visit('http://localhost:3000/generate-tests');
   });
 
-  it('Can generate all expected rows successfully', () => {
-    cy.get('.generateTest').each(($element) => {
-      const className = $element.attr('id').replace('generateTest', '');
-      cy.wrap($element).within(() => {
+  it('verifies all tests are categorized', () => {
+    const missingCases: string[] = [];
+    cy.get('.generateTest')
+      .each(($element) => {
+        const testCase = $element.attr('id').replace('generateTest', '');
+        if (
+          !EXPECTED_SUCCESSFUL_CASES.has(testCase) &&
+          !EXPECTED_INVALID_INPUT_CASES.has(testCase) &&
+          !EXPECTED_INTERNAL_ERROR_CASES.has(testCase)
+        ) {
+          missingCases.push(testCase);
+        }
+      })
+      .then(() => {
+        if (missingCases.length > 0) {
+          throw new Error(
+            // eslint-disable-next-line max-len
+            `Expected all tests cases to be mapped, didn't find config for ${missingCases}. Please add them to EXPECTED_INVALID_INPUT_CASES, EXPECTED_INTERNAL_ERROR_CASES, or EXPECTED_SUCCESSFUL_CASES`,
+          );
+        }
+      });
+  });
+
+  EXPECTED_SUCCESSFUL_CASES.forEach((testCase) => {
+    it(`Generates a successful result for ${testCase}`, () => {
+      cy.get(`#generateTest${testCase}`).within(() => {
         cy.get('button').click();
         TARGET_GENERATORS.forEach((targetName) => {
-          if (EXPECTED_INVALID_INPUT_CASES.has(className)) {
-            cy.get(`.${targetName}`).contains('❌');
-            cy.get(`.${targetName}`).contains('InvalidInputError');
-          } else if (EXPECTED_INTERNAL_ERROR_CASES.has(className)) {
-            cy.get(`.${targetName}`).contains('❌');
-            cy.get(`.${targetName}`).contains('InternalError');
-          } else {
-            cy.get(`.${targetName}`).contains('✅');
-          }
+          cy.get(`.${targetName}`).contains('✅');
+        });
+      });
+    });
+  });
+
+  EXPECTED_INVALID_INPUT_CASES.forEach((testCase) => {
+    it(`Generates an invalid input result for ${testCase}`, () => {
+      cy.get(`#generateTest${testCase}`).within(() => {
+        cy.get('button').click();
+        TARGET_GENERATORS.forEach((targetName) => {
+          cy.get(`.${targetName}`).contains('❌');
+          cy.get(`.${targetName}`).contains('InvalidInputError');
+        });
+      });
+    });
+  });
+
+  EXPECTED_INTERNAL_ERROR_CASES.forEach((testCase) => {
+    it(`Generates an internal error result for ${testCase}`, () => {
+      cy.get(`#generateTest${testCase}`).within(() => {
+        cy.get('button').click();
+        TARGET_GENERATORS.forEach((targetName) => {
+          cy.get(`.${targetName}`).contains('❌');
+          cy.get(`.${targetName}`).contains('InternalError');
         });
       });
     });
