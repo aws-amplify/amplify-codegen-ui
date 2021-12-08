ebc8918361c094f7c9c4dea58d14b42903bde3ad
Alexander Harris <ademp90@gmail.com>
2021-12-07 22:20:36 -0800

    test: add e2e test app files, and full cypress suite to check

---
+++ packages/test-generator/e2e-test-templates/App.js
@@ -0,0 +1,33 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import { AmplifyProvider } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+import { NavBar, BlogPosts } from './ui-components';
+
+Amplify.configure(awsconfig);
+
+function App() {
+  return (
+    <AmplifyProvider>
+      <NavBar id="navBar" />
+      <BlogPosts id="blogPosts" />
+    </AmplifyProvider>
+  );
+}
+
+export default App;
