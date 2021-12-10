de88d068577a99ec25b9d523cb235e029dd7add2
Al Harris <91494052+alharris-at@users.noreply.github.com>
2021-12-10 11:46:14 -0800

    chore: add golden json files to update snapshots in studio repo (#298)

---
+++ packages/test-generator/lib/components/golden-files/index.ts
@@ -0,0 +1,36 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+/* eslint-disable max-len */
+export { default as GoldenBasicComponent } from './golden-basic-component.json';
+export { default as GoldenCollectionWithDataBindingAndPagination } from './golden-collection-with-data-binding-and-pagination.json';
+export { default as GoldenCollectionWithDataBindingAndSort } from './golden-collection-with-data-binding-and-sort.json';
+export { default as GoldenCollectionWithDataBinding } from './golden-collection-with-data-binding.json';
+export { default as GoldenCollectionWithSearchAndPagination } from './golden-collection-with-search-and-pagination.json';
+export { default as GoldenCollectionWithSpecificRecord } from './golden-collection-with-specific-record.json';
+export { default as GoldenComponentWithAuthAttributes } from './golden-component-with-auth-attributes.json';
+export { default as GoldenComponentWithChildrenAndDataBinding } from './golden-component-with-children-and-data-binding.json';
+export { default as GoldenComponentWithChildren } from './golden-component-with-children.json';
+export { default as GoldenComponentWithCustomChildren } from './golden-component-with-custom-children.json';
+export { default as GoldenComponentWithConcatAndConditional } from './golden-component-with-concat-and-conditional.json';
+export { default as GoldenComponentWithConditionalWithoutField } from './golden-component-with-conditional-without-field.json';
+export { default as GoldenComponentWithDataBindingAndDatastoreDefault } from './golden-component-with-data-binding-and-datastore-default.json';
+export { default as GoldenComponentWithDataBinding } from './golden-component-with-data-binding.json';
+export { default as GoldenComponentWithEvent } from './golden-component-with-event.json';
+export { default as GoldenComponentWithExposed } from './golden-component-with-exposed.json';
+export { default as GoldenComponentWithForm } from './golden-component-with-form.json';
+export { default as GoldenComponentWithImageWithStorage } from './golden-component-with-image-with-storage.json';
+export { default as GoldenComponentWithTypedProp } from './golden-component-with-typed-prop.json';
+export { default as GoldenComponentWithVariants } from './golden-component-with-variants.json';
