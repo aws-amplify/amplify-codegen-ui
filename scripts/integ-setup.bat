ce2c57c40f01785f90bb4d954069d35e791f230a
Dane Pilcher <dppilche@amazon.com>
2021-12-14 14:02:38 -0700

    ci: pin react-scripts@4.0.3 (#308)

--- scripts/integ-setup.bat
+++ scripts/integ-setup.bat
@@ -4,7 +4,7 @@ call npm run build
 
 :: create
 chdir packages
-call npx create-react-app integration-test --use-npm --template typescript
+call npx create-react-app integration-test --use-npm --template typescript --scripts-version 4.0.3
 chdir ..
 
 :: add files
