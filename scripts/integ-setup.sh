67bce4880c80cebfaf7f661bfe1d79c53c3ac27d
Dane Pilcher <dppilche@amazon.com>
2022-01-05 08:19:08 -0700

    ci: unpin react-scripts (#314)
    * ci: unpin react-scripts
    
    This reverts commit ce2c57c40f01785f90bb4d954069d35e791f230a.
    
    * ci: fix webpack 5

--- scripts/integ-setup.sh
+++ scripts/integ-setup.sh
@@ -4,7 +4,7 @@ npm run integ:clean
 npm run build
 
 # create
-(cd packages && npx create-react-app integration-test --use-npm --template typescript --scripts-version 4.0.3)
+(cd packages && npx create-react-app integration-test --use-npm --template typescript)
 
 # add files
 npm run integ:templates
@@ -21,3 +21,11 @@ lerna add --no-ci --scope integration-test react-router-dom
 lerna add --no-ci --scope integration-test @types/react-router-dom
 lerna add --no-ci --dev --scope integration-test cypress
 lerna add --no-ci --dev --scope integration-test wait-on
+lerna add --no-ci --scope integration-test os-browserify
+lerna add --no-ci --scope integration-test path-browserify
+lerna add --no-ci --scope integration-test react-app-rewired
+
+(cd packages/integration-test && \
+  jq '.scripts.start = "react-app-rewired start" | .scripts.build = "react-app-rewired build" | .scripts.test = "react-app-rewired test"' package.json > tmp.json && \
+  mv tmp.json package.json \
+)
