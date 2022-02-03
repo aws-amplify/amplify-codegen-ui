0870304d22aff6ee0413cda1b3019b7e8d811892
Al Harris <91494052+alharris-at@users.noreply.github.com>
2022-02-03 08:23:22 -0800

    fix: support synthetic prop mutation, and duplicate mutations on the same property (#375)

---
+++ packages/codegen-ui-react/lib/workflow/utils.ts
@@ -0,0 +1,31 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+import { InvalidInputError } from '@aws-amplify/codegen-ui';
+import Primitive, { PrimitiveChildrenPropMapping } from '../primitive';
+
+export const getChildPropMappingForComponentName = (
+  componentNameToTypeMap: Record<string, string>,
+  componentName: string,
+): string | undefined => {
+  const referencedComponentType = componentNameToTypeMap[componentName];
+  if (!referencedComponentType) {
+    throw new InvalidInputError(
+      `Invalid definition, found reference to component name ${componentName} which wasn't found in the schema.`,
+    );
+  }
+  return PrimitiveChildrenPropMapping[Primitive[referencedComponentType as Primitive]];
+};
