7eaa97878db3757a722dfd36fd2d827df28475a2
Alexander Harris <alharris@amazon.com>
2022-02-10 10:10:31 -0800

    chore: adding mutation binding tests

---
+++ packages/test-generator/integration-test-templates/cypress/integration/action-binding-spec.ts
@@ -0,0 +1,99 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+describe('Workflow', () => {
+  before(() => {
+    cy.visit('http://localhost:3000/action-binding-tests');
+  });
+
+  describe('Mutation Bindings', () => {
+    it('supports value bindings', () => {
+      cy.get('#mutated-value').contains('Fixed Value').should('not.exist');
+      cy.contains('Apply Fixed Property Mutation').click();
+      cy.get('#mutated-value').contains('Fixed Value');
+    });
+
+    it('supports bound prop bindings', () => {
+      cy.get('#mutated-value').contains('Bound Value').should('not.exist');
+      cy.contains('Apply Bound Property Mutation').click();
+      cy.get('#mutated-value').contains('Bound Value');
+    });
+
+    it('supports concat bindings', () => {
+      cy.get('#mutated-value').contains('Concatenated Value').should('not.exist');
+      cy.contains('Apply Concatenated Property Mutation').click();
+      cy.get('#mutated-value').contains('Concatenated Value');
+    });
+
+    it('supports conditional bindings', () => {
+      cy.get('#mutated-value').contains('Conditional Value').should('not.exist');
+      cy.contains('Apply Conditional Property Mutation').click();
+      cy.get('#mutated-value').contains('Conditional Value');
+    });
+
+    // Auth Bindings not yet supported in functional tests
+    it.skip('supports auth bindings', () => {
+      cy.get('#mutated-value').contains('Auth Value').should('not.exist');
+      cy.contains('Apply Auth Property Mutation').click();
+      cy.get('#mutated-value').contains('Auth Value');
+    });
+
+    it('supports state bindings', () => {
+      cy.get('#mutated-value').contains('State Value').should('not.exist');
+      cy.contains('Apply State Property Mutation').click();
+      cy.get('#mutated-value').contains('State Value');
+    });
+  });
+
+  describe('DataStore Bindings', () => {
+    it('supports value bindings', () => {
+      cy.get('#data-store-value').contains('Fixed Value').should('not.exist');
+      cy.contains('Apply Fixed Property DataStoreUpdateItemAction').click();
+      cy.get('#data-store-value').contains('Fixed Value');
+    });
+
+    it('supports bound prop bindings', () => {
+      cy.get('#data-store-value').contains('Bound Value').should('not.exist');
+      cy.contains('Apply Bound Property DataStoreUpdateItemAction').click();
+      cy.get('#data-store-value').contains('Bound Value');
+    });
+
+    it('supports concat bindings', () => {
+      cy.get('#data-store-value').contains('Concatenated Value').should('not.exist');
+      cy.contains('Apply Concatenated Property DataStoreUpdateItemAction').click();
+      cy.get('#data-store-value').contains('Concatenated Value');
+    });
+
+    it('supports conditional bindings', () => {
+      cy.get('#data-store-value').contains('Conditional Value').should('not.exist');
+      cy.contains('Apply Conditional Property DataStoreUpdateItemAction').click();
+      cy.get('#data-store-value').contains('Conditional Value');
+    });
+
+    // Auth Bindings not yet supported in functional tests
+    it.skip('supports auth bindings', () => {
+      cy.get('#data-store-value').contains('Auth Value').should('not.exist');
+      cy.contains('Apply Auth Property DataStoreUpdateItemAction').click();
+      cy.get('#data-store-value').contains('Auth Value');
+    });
+
+    it('supports state bindings', () => {
+      cy.get('#data-store-value').contains('State Value').should('not.exist');
+      cy.contains('Apply State Property DataStoreUpdateItemAction').click();
+      cy.get('#data-store-value').contains('State Value');
+    });
+  });
+});
