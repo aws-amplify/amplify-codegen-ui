c66dbffeb29e9f481284e8af15480dcbda9611eb
Dane Pilcher <dppilche@amazon.com>
2022-02-14 17:07:00 +0000

    fix: remove defaultValue if value is added to component

--- packages/codegen-ui-react/lib/react-component-with-children-renderer.ts
+++ packages/codegen-ui-react/lib/react-component-with-children-renderer.ts
@@ -33,7 +33,6 @@ import {
   buildOpeningElementEvents,
   filterStateReferencesForComponent,
 } from './workflow';
-import Primitive, { PrimitiveChildrenPropMapping } from './primitive';
 
 export class ReactComponentWithChildrenRenderer<TPropIn> extends ComponentWithChildrenRendererBase<
   TPropIn,
@@ -47,7 +46,6 @@ export class ReactComponentWithChildrenRenderer<TPropIn> extends ComponentWithCh
     protected parent?: StudioNode,
   ) {
     super(component, parent);
-    this.mapSyntheticProps();
     addBindingPropertiesImports(component, importCollection);
   }
 
@@ -193,27 +191,5 @@ export class ReactComponentWithChildrenRenderer<TPropIn> extends ComponentWithCh
     );
 
     attributes.push(attr);
-  }
-
-  /* Some additional props are added to Amplify primitives in Studio. These "sythetic" props are mapped to real props
-   * on the primitives.
-   *
-   * Example: Text prop label is mapped to to Text prop Children
-   *
-   * This is done so that nonadvanced users of Studio do not need to interact with props that might confuse them.
-   */
-  private mapSyntheticProps() {
-    // properties.children will take precedent over mapped children prop
-    if (this.component.properties.children === undefined) {
-      const childrenPropMapping = PrimitiveChildrenPropMapping[Primitive[this.component.componentType as Primitive]];
-
-      if (childrenPropMapping !== undefined) {
-        const mappedChildrenProp = this.component.properties[childrenPropMapping];
-        if (mappedChildrenProp !== undefined) {
-          this.component.properties.children = mappedChildrenProp;
-          delete this.component.properties[childrenPropMapping];
-        }
-      }
-    }
   }
 }
