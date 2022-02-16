a7b7c5f440ce53cf737d1b72b06af902b44457f7
Alexander Harris <alharris@amazon.com>
2022-02-15 18:32:07 -0800

    chore: add functional test with complex serialized datastore items

--- packages/test-generator/lib/components/workflow/index.ts
+++ packages/test-generator/lib/components/workflow/index.ts
@@ -28,3 +28,4 @@ export { default as ConditionalInMutation } from './conditionalInMutation.json';
 export { default as TwoWayBindings } from './twoWayBindings.json';
 export { default as MutationActionBindings } from './mutationActionBindings.json';
 export { default as DataStoreActionBindings } from './dataStoreActionBindings.json';
+export { default as CreateModelWithComplexTypes } from './createModelWithComplexTypes.json';
