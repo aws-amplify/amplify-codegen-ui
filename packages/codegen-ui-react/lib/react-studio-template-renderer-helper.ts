b07b704952b1c9f798b93cd552556aa864859fd2
Alexander Harris <alharris@amazon.com>
2022-02-18 14:49:12 -0800

    feat: try and marshall variant values to json objects to support icon viewBox/paths

--- packages/codegen-ui-react/lib/react-studio-template-renderer-helper.ts
+++ packages/codegen-ui-react/lib/react-studio-template-renderer-helper.ts
@@ -151,6 +151,7 @@ export type json = string | number | boolean | null | json[] | { [key: string]: 
 // eslint-disable-next-line consistent-return
 export function jsonToLiteral(
   jsonObject: json,
+  tryAndMarshallStringsToObjects?: boolean,
 ): ObjectLiteralExpression | StringLiteral | NumericLiteral | BooleanLiteral | NullLiteral | ArrayLiteralExpression {
   if (jsonObject === null) {
     return factory.createNull();
@@ -158,6 +159,14 @@ export function jsonToLiteral(
   // eslint-disable-next-line default-case
   switch (typeof jsonObject) {
     case 'string':
+      if (tryAndMarshallStringsToObjects) {
+        try {
+          const parsedObject = JSON.parse(jsonObject);
+          if (typeof parsedObject === 'object') {
+            return jsonToLiteral(parsedObject, tryAndMarshallStringsToObjects);
+          }
+        } catch (e) {} // eslint-disable-line no-empty
+      }
       return factory.createStringLiteral(jsonObject);
     case 'number':
       return factory.createNumericLiteral(jsonObject);
@@ -169,12 +178,18 @@ export function jsonToLiteral(
     }
     case 'object': {
       if (jsonObject instanceof Array) {
-        return factory.createArrayLiteralExpression(jsonObject.map(jsonToLiteral), false);
+        return factory.createArrayLiteralExpression(
+          jsonObject.map((obj) => jsonToLiteral(obj, tryAndMarshallStringsToObjects)),
+          false,
+        );
       }
       // else object
       return factory.createObjectLiteralExpression(
         Object.entries(jsonObject).map(([key, value]) =>
-          factory.createPropertyAssignment(factory.createStringLiteral(key), jsonToLiteral(value)),
+          factory.createPropertyAssignment(
+            factory.createStringLiteral(key),
+            jsonToLiteral(value, tryAndMarshallStringsToObjects),
+          ),
         ),
         false,
       );
