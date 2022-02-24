7244ec2bcb7f0dcf4a11b8619f93a9fe249e88bf
Alexander Harris <alharris@amazon.com>
2022-02-24 15:00:39 -0800

    chore: unpin dependencies, point to updated ui-react version w/ bugfix

--- packages/codegen-ui-react/lib/react-required-dependency-provider.ts
+++ packages/codegen-ui-react/lib/react-required-dependency-provider.ts
@@ -24,7 +24,7 @@ export class ReactRequiredDependencyProvider extends RequiredDependencyProvider<
     return [
       {
         dependencyName: '@aws-amplify/ui-react',
-        supportedSemVerPattern: '^2.6.1',
+        supportedSemVerPattern: '^2.6.2',
         reason: 'Required to leverage amplify ui primitives, and studio component helper functions.',
       },
     ];
