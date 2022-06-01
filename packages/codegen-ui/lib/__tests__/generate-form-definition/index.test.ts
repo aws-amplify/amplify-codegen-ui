69490acf1fa6e66d37d68a97a8682c6f3763a3ec
Hein Jeong <73264629+hein-j@users.noreply.github.com>
2022-06-01 09:01:21 -0700

    chore: add form definition mapper (#470)
    Co-authored-by: Hein Jeong <heinje@amazon.com>

---
+++ packages/codegen-ui/lib/__tests__/generate-form-definition/index.test.ts
@@ -0,0 +1,225 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+import { generateFormDefinition } from '../../generate-form-definition';
+
+describe('generateFormDefinition', () => {
+  it('should map DataStore model fields', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: {},
+        sectionalElements: [],
+        style: {},
+      },
+      modelInfo: { fields: [{ name: 'name', type: 'String', isReadOnly: false, isRequired: true, isArray: false }] },
+    });
+    expect(formDefinition.elements).toStrictEqual({
+      name: {
+        componentType: 'TextField',
+        props: { label: 'name', isRequired: true, isReadOnly: false },
+        dataType: 'String',
+      },
+    });
+  });
+
+  it('should override field configurations from DataStore', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: { weight: { inputType: { type: 'SliderField', minValue: 1, maxValue: 100, step: 2 } } },
+        sectionalElements: [],
+        style: {},
+      },
+      modelInfo: { fields: [{ name: 'weight', type: 'Float', isReadOnly: false, isRequired: true, isArray: false }] },
+    });
+    expect(formDefinition.elements).toStrictEqual({
+      weight: {
+        componentType: 'SliderField',
+        props: { label: 'weight', minValue: 1, maxValue: 100, step: 2, isReadOnly: false, isRequired: true },
+        dataType: 'Float',
+      },
+    });
+  });
+
+  it('should not add overrides to the matrix', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: { weight: { inputType: { type: 'SliderField', minValue: 1, maxValue: 100, step: 2 } } },
+        sectionalElements: [],
+        style: {},
+      },
+      modelInfo: { fields: [{ name: 'weight', type: 'Float', isReadOnly: false, isRequired: true, isArray: false }] },
+    });
+
+    expect(formDefinition.elementMatrix).toStrictEqual([['weight']]);
+  });
+
+  it('should add fields that do not exist in DataStore', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: { weight: { inputType: { type: 'SliderField', minValue: 1, maxValue: 100, step: 2 } } },
+        sectionalElements: [],
+        style: {},
+      },
+      modelInfo: { fields: [] },
+    });
+    expect(formDefinition.elements).toStrictEqual({
+      weight: { componentType: 'SliderField', props: { minValue: 1, maxValue: 100, step: 2 } },
+    });
+  });
+
+  it('should add fields that do not exist in DataStore to the matrix', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: { weight: { inputType: { type: 'SliderField', minValue: 1, maxValue: 100, step: 2 } } },
+        sectionalElements: [],
+        style: {},
+      },
+      modelInfo: { fields: [] },
+    });
+    expect(formDefinition.elementMatrix).toStrictEqual([['weight']]);
+  });
+
+  it('should add sectional elements', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: {},
+        sectionalElements: [
+          { type: 'Heading', name: 'Heading123', position: { fixed: 'first' }, level: 1, text: 'Create Dog' },
+        ],
+        style: {},
+      },
+      modelInfo: { fields: [{ name: 'weight', type: 'Float', isReadOnly: false, isRequired: true, isArray: false }] },
+    });
+    expect(formDefinition.elements.Heading123).toStrictEqual({
+      componentType: 'Heading',
+      props: { level: 1, text: 'Create Dog' },
+    });
+  });
+
+  it('should apply layout style', () => {
+    const style = { verticalGap: { value: '10px' }, horizontalGap: { tokenReference: 'color.primary.solid' } };
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: {},
+        sectionalElements: [],
+        style,
+      },
+      modelInfo: { fields: [] },
+    });
+    expect(formDefinition.form.props.layoutStyle).toStrictEqual(style);
+  });
+
+  it('should not leave empty rows in the matrix', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: {
+          weight: { excluded: true },
+          name: { excluded: true },
+          age: { excluded: true },
+        },
+        sectionalElements: [
+          { type: 'Heading', name: 'Heading123', position: { fixed: 'first' }, level: 1, text: 'Create Dog' },
+        ],
+        style: {},
+      },
+      modelInfo: {
+        fields: [
+          { name: 'name', type: 'String', isReadOnly: false, isRequired: true, isArray: false },
+          { name: 'weight', type: 'Float', isReadOnly: false, isRequired: true, isArray: false },
+          { name: 'age', type: 'Int', isReadOnly: false, isRequired: true, isArray: false },
+        ],
+      },
+    });
+    expect(formDefinition.elementMatrix).toStrictEqual([['Heading123']]);
+  });
+
+  it('should correctly map positions', () => {
+    const formDefinition = generateFormDefinition({
+      form: {
+        dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+        fields: {
+          weight: { position: { rightOf: 'age' } },
+          name: { position: { below: 'Heading123' } },
+          age: { position: { rightOf: 'name' } },
+        },
+        sectionalElements: [
+          { type: 'Heading', name: 'Heading123', position: { fixed: 'first' }, level: 1, text: 'Create Dog' },
+        ],
+        style: {},
+      },
+      modelInfo: {
+        fields: [
+          { name: 'name', type: 'String', isReadOnly: false, isRequired: true, isArray: false },
+          { name: 'weight', type: 'Float', isReadOnly: false, isRequired: true, isArray: false },
+          { name: 'age', type: 'Int', isReadOnly: false, isRequired: true, isArray: false },
+        ],
+      },
+    });
+    expect(formDefinition.elementMatrix).toStrictEqual([['Heading123'], ['name', 'age', 'weight']]);
+  });
+});
+
+it('should requeue if related element is not yet found', () => {
+  const formDefinition = generateFormDefinition({
+    form: {
+      dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+      fields: {
+        color: { position: { below: 'name' } },
+        weight: { position: { rightOf: 'age' } },
+        age: { position: { rightOf: 'name' } },
+        name: { position: { below: 'Heading123' } },
+      },
+      sectionalElements: [
+        { type: 'Heading', name: 'Heading123', position: { fixed: 'first' }, level: 1, text: 'Create Dog' },
+      ],
+      style: {},
+    },
+    modelInfo: {
+      fields: [],
+    },
+  });
+  expect(formDefinition.elementMatrix).toStrictEqual([['Heading123'], ['name', 'age', 'weight'], ['color']]);
+});
+
+it('should handle fields without position', () => {
+  const formDefinition = generateFormDefinition({
+    form: {
+      dataType: { dataSourceType: 'DataStore', dataTypeName: 'Dog' },
+      fields: {
+        color: { position: { below: 'name' } },
+        weight: { position: { rightOf: 'age' } },
+        age: { position: { rightOf: 'name' } },
+        name: { label: 'Name' },
+        bark: { label: 'Bark' },
+      },
+      sectionalElements: [
+        { type: 'Heading', name: 'Heading123', position: { fixed: 'first' }, level: 1, text: 'Create Dog' },
+      ],
+      style: {},
+    },
+    modelInfo: {
+      fields: [],
+    },
+  });
+  expect(formDefinition.elementMatrix).toStrictEqual([['Heading123'], ['name', 'age', 'weight'], ['color'], ['bark']]);
+});
---
+++ packages/codegen-ui/lib/generate-form-definition/helpers/form-field.ts
@@ -0,0 +1,87 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+import { FormDefinition, StudioFormFieldConfig } from '../../types';
+import { FIELD_TYPE_MAP } from './field-type-map';
+import { InvalidInputError } from '../../errors';
+
+/**
+ * Impure function that adds field configurations to formDefinition
+ */
+/* eslint-disable no-param-reassign */
+export function mapFormFieldConfig(
+  element: { type: string; name: string; config: StudioFormFieldConfig },
+  formDefinition: FormDefinition,
+) {
+  const { config } = element;
+
+  if ('label' in config && config.label) {
+    formDefinition.elements[element.name] = {
+      ...formDefinition.elements[element.name],
+      props: {
+        ...formDefinition.elements[element.name]?.props,
+        label: config.label,
+      },
+    };
+  }
+
+  if ('inputType' in config && config.inputType) {
+    const dataType = formDefinition.elements[element.name]?.dataType;
+
+    if (dataType) {
+      if (
+        FIELD_TYPE_MAP[dataType].defaultComponent !== config.inputType.type &&
+        !FIELD_TYPE_MAP[dataType].supportedComponents?.has(config.inputType.type)
+      ) {
+        throw new InvalidInputError(
+          `The input type ${config.inputType.type} is not supported for data type ${dataType}`,
+        );
+      }
+    }
+
+    const newProps = { ...formDefinition.elements[element.name]?.props };
+
+    if ('required' in config.inputType) {
+      newProps.isRequired = config.inputType.required;
+    }
+
+    if ('readOnly' in config.inputType) {
+      newProps.isReadOnly = config.inputType.readOnly;
+    }
+
+    if ('placeholder' in config.inputType) {
+      newProps.placeholder = config.inputType.placeholder;
+    }
+
+    if ('minValue' in config.inputType) {
+      newProps.minValue = config.inputType.minValue;
+    }
+
+    if ('maxValue' in config.inputType) {
+      newProps.maxValue = config.inputType.maxValue;
+    }
+
+    if ('step' in config.inputType) {
+      newProps.step = config.inputType.step;
+    }
+
+    formDefinition.elements[element.name] = {
+      ...formDefinition.elements[element.name],
+      componentType: config.inputType.type,
+      props: newProps,
+    };
+  }
+}
+/* eslint-enable no-param-reassign */
