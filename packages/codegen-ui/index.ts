91aa8ccf02bf0ec6634d21b7d6c2a53a47176d8f
Hein Jeong <heinje@amazon.com>
2022-07-05 22:09:06 +0000

    chore: add util to map datastore schema to generic

--- packages/codegen-ui/index.ts
+++ packages/codegen-ui/index.ts
@@ -21,6 +21,7 @@ export * from './lib/render-component-response';
 export * from './lib/framework-output-manager';
 export * from './lib/template-renderer-factory';
 export * from './lib/generate-form-definition';
+export * from './lib/generic-from-datastore';
 
 export * from './lib/renderer-helper';
 export * from './lib/validation-helper';
--- packages/codegen-ui/lib/generate-form-definition/form-to-component.ts
+++ packages/codegen-ui/lib/generate-form-definition/form-to-component.ts
@@ -13,10 +13,8 @@
   See the License for the specific language governing permissions and
   limitations under the License.
  */
+import { SchemaModel, ModelFields, isGraphQLScalarType } from '@aws-amplify/datastore';
 import {
-  SchemaModel,
-  ModelFields,
-  isGraphQLScalarType,
   StudioComponent,
   StudioForm,
   StudioComponentChild,
