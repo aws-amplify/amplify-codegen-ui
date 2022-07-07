7fdd7a0c9d9e4dd46ee4fb6a9c36966237a364c9
Hein Jeong <73264629+hein-j@users.noreply.github.com>
2022-07-07 09:13:51 -0700

    chore: add util to map datastore schema to generic (#498)
    Co-authored-by: Hein Jeong <heinje@amazon.com>

--- packages/codegen-ui/lib/__tests__/__utils__/mock-schemas.ts
+++ packages/codegen-ui/lib/__tests__/__utils__/mock-schemas.ts
@@ -112,3 +112,674 @@ export const postSchema: Schema = {
   nonModels: {},
   version: '000000',
 };
+
+export const schemaWithRelationships: Schema = {
+  models: {
+    PrimaryCareGiver: {
+      name: 'PrimaryCareGiver',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: 'name',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        Child: {
+          name: 'Child',
+          isArray: false,
+          type: {
+            model: 'Child',
+          },
+          isRequired: false,
+          attributes: [],
+          association: {
+            connectionType: 'HAS_ONE',
+            associatedWith: 'id',
+            targetName: 'primaryCareGiverChildId',
+          },
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        primaryCareGiverChildId: {
+          name: 'primaryCareGiverChildId',
+          isArray: false,
+          type: 'ID',
+          isRequired: false,
+          attributes: [],
+        },
+      },
+      syncable: true,
+      pluralName: 'PrimaryCareGivers',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    Child: {
+      name: 'Child',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: 'name',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'Children',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    Student: {
+      name: 'Student',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: 'name',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        Teachers: {
+          name: 'Teachers',
+          isArray: true,
+          type: {
+            model: 'StudentTeacher',
+          },
+          isRequired: false,
+          attributes: [],
+          isArrayNullable: true,
+          association: {
+            connectionType: 'HAS_MANY',
+            associatedWith: 'student',
+          },
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'Students',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    Teacher: {
+      name: 'Teacher',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: 'name',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        students: {
+          name: 'students',
+          isArray: true,
+          type: {
+            model: 'StudentTeacher',
+          },
+          isRequired: false,
+          attributes: [],
+          isArrayNullable: true,
+          association: {
+            connectionType: 'HAS_MANY',
+            associatedWith: 'teacher',
+          },
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'Teachers',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    Lock: {
+      name: 'Lock',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        Key: {
+          name: 'Key',
+          isArray: false,
+          type: {
+            model: 'Key',
+          },
+          isRequired: false,
+          attributes: [],
+          association: {
+            connectionType: 'HAS_ONE',
+            associatedWith: 'Lock',
+            targetName: 'lockKeyId',
+          },
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        lockKeyId: {
+          name: 'lockKeyId',
+          isArray: false,
+          type: 'ID',
+          isRequired: false,
+          attributes: [],
+        },
+      },
+      syncable: true,
+      pluralName: 'Locks',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    Key: {
+      name: 'Key',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: 'name',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        Lock: {
+          name: 'Lock',
+          isArray: false,
+          type: {
+            model: 'Lock',
+          },
+          isRequired: false,
+          attributes: [],
+          association: {
+            connectionType: 'BELONGS_TO',
+            targetName: 'keyLockId',
+          },
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'Keys',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    Owner: {
+      name: 'Owner',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: 'name',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        Dog: {
+          name: 'Dog',
+          isArray: true,
+          type: {
+            model: 'Dog',
+          },
+          isRequired: false,
+          attributes: [],
+          isArrayNullable: true,
+          association: {
+            connectionType: 'HAS_MANY',
+            associatedWith: 'ownerID',
+          },
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'Owners',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    Dog: {
+      name: 'Dog',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: 'name',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        ownerID: {
+          name: 'ownerID',
+          isArray: false,
+          type: 'ID',
+          isRequired: false,
+          attributes: [],
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'Dogs',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'key',
+          properties: {
+            name: 'byOwner',
+            fields: ['ownerID'],
+          },
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+    StudentTeacher: {
+      name: 'StudentTeacher',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        student: {
+          name: 'student',
+          isArray: false,
+          type: {
+            model: 'Student',
+          },
+          isRequired: true,
+          attributes: [],
+          association: {
+            connectionType: 'BELONGS_TO',
+            targetName: 'studentID',
+          },
+        },
+        teacher: {
+          name: 'teacher',
+          isArray: false,
+          type: {
+            model: 'Teacher',
+          },
+          isRequired: true,
+          attributes: [],
+          association: {
+            connectionType: 'BELONGS_TO',
+            targetName: 'teacherID',
+          },
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'StudentTeachers',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'key',
+          properties: {
+            name: 'byStudent',
+            fields: ['studentID'],
+          },
+        },
+        {
+          type: 'key',
+          properties: {
+            name: 'byTeacher',
+            fields: ['teacherID'],
+          },
+        },
+      ],
+    },
+  },
+  enums: {},
+  nonModels: {},
+  version: '3ea7de9ef8e765b48c0a53e3e45735a3',
+};
+
+export const schemaWithEnums: Schema = {
+  models: {},
+  enums: {
+    City: {
+      name: 'City',
+      values: ['SAN_FRANCISCO', 'NEW_YORK'],
+    },
+  },
+  nonModels: {},
+  version: '3ea7de9ef8e765b48c0a53e3e45735a3',
+};
+
+export const schemaWithNonModels: Schema = {
+  models: {},
+  enums: {},
+  nonModels: {
+    Reactions: {
+      name: 'Reactions',
+      fields: {
+        ball: {
+          name: 'ball',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        fireworks: {
+          name: 'fireworks',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+      },
+    },
+    Misc: {
+      name: 'Misc',
+      fields: {
+        quotes: {
+          name: 'quotes',
+          isArray: true,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+      },
+    },
+  },
+  version: '38a1a46479c6cd75d21439d7f3122c1d',
+};
---
+++ packages/codegen-ui/lib/generic-from-datastore.ts
@@ -0,0 +1,145 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+import { Schema as DataStoreSchema, ModelField } from '@aws-amplify/datastore';
+import { InvalidInputError } from './errors';
+import { GenericDataField, GenericDataSchema } from './types';
+
+function getGenericDataField(field: ModelField): GenericDataField {
+  return {
+    dataType: field.type,
+    required: !!field.isRequired,
+    readOnly: !!field.isReadOnly,
+    isArray: field.isArray,
+  };
+}
+
+function addRelationship(
+  fields: {
+    [modelName: string]: { [fieldName: string]: GenericDataField['relationship'] };
+  },
+  modelName: string,
+  fieldName: string,
+  relationship: GenericDataField['relationship'],
+) {
+  // handle prototype-pollution vulnerability
+  if (modelName === '__proto__') {
+    throw new InvalidInputError('Invalid model name "__proto__"');
+  }
+  if (!fields[modelName]) {
+    // eslint-disable-next-line no-param-reassign
+    fields[modelName] = {};
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  fields[modelName][fieldName] = relationship;
+}
+
+export function getGenericFromDataStore(dataStoreSchema: DataStoreSchema): GenericDataSchema {
+  const genericSchema: GenericDataSchema = {
+    dataSourceType: 'DataStore',
+    models: {},
+    enums: {},
+    nonModels: {},
+  };
+
+  const fieldsWithImplicitRelationships: {
+    [modelName: string]: { [fieldName: string]: GenericDataField['relationship'] };
+  } = {};
+
+  Object.values(dataStoreSchema.models).forEach((model) => {
+    const genericFields: { [fieldName: string]: GenericDataField } = {};
+
+    Object.values(model.fields).forEach((field) => {
+      const genericField = getGenericDataField(field);
+
+      // handle relationships
+      if (typeof field.type === 'object' && 'model' in field.type) {
+        if (field.association) {
+          const relationshipType = field.association.connectionType;
+
+          let relatedModelName = field.type.model;
+
+          if (relationshipType === 'HAS_MANY' && 'associatedWith' in field.association) {
+            const associatedModel = dataStoreSchema.models[field.type.model];
+            const associatedFieldName = field.association.associatedWith;
+            const associatedField = associatedModel.fields[associatedFieldName];
+
+            // if the associated model is a join table, update relatedModelName to the actual related model
+            if (
+              typeof associatedField.type === 'object' &&
+              'model' in associatedField.type &&
+              associatedField.type.model === model.name
+            ) {
+              const relatedJoinField = Object.values(associatedModel.fields).find(
+                (joinField) =>
+                  joinField.name !== associatedFieldName &&
+                  typeof joinField.type === 'object' &&
+                  'model' in joinField.type,
+              );
+              if (relatedJoinField && typeof relatedJoinField.type === 'object' && 'model' in relatedJoinField.type) {
+                relatedModelName = relatedJoinField.type.model;
+              }
+              // if the associated model is not a join table, note implicit relationship for associated field
+            } else {
+              addRelationship(fieldsWithImplicitRelationships, associatedModel.name, associatedFieldName, {
+                type: 'HAS_ONE',
+                relatedModelName: model.name,
+              });
+            }
+          }
+
+          // note implicit relationship for associated field within same model
+          if (relationshipType === 'HAS_ONE' && 'targetName' in field.association && field.association.targetName) {
+            addRelationship(fieldsWithImplicitRelationships, model.name, field.association.targetName, {
+              type: relationshipType,
+              relatedModelName,
+            });
+          }
+
+          genericField.relationship = { type: relationshipType, relatedModelName };
+        }
+      }
+
+      genericFields[field.name] = genericField;
+    });
+
+    genericSchema.models[model.name] = { fields: genericFields };
+  });
+
+  Object.entries(fieldsWithImplicitRelationships).forEach(([modelName, fields]) => {
+    Object.entries(fields).forEach(([fieldName, relationship]) => {
+      const field = genericSchema.models[modelName]?.fields[fieldName];
+      if (field) {
+        field.relationship = relationship;
+      }
+    });
+  });
+
+  genericSchema.enums = dataStoreSchema.enums;
+
+  if (dataStoreSchema.nonModels) {
+    Object.values(dataStoreSchema.nonModels).forEach((nonModel) => {
+      const genericFields: { [fieldName: string]: GenericDataField } = {};
+      Object.values(nonModel.fields).forEach((field) => {
+        const genericField = getGenericDataField(field);
+        genericFields[field.name] = genericField;
+      });
+      genericSchema.nonModels[nonModel.name] = { fields: genericFields };
+    });
+  }
+
+  return genericSchema;
+}
