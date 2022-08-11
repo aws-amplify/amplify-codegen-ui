e118af34e2cf5ffe49f8611d8eb039fd67cb889a
David Lopez <lopezbnd@amazon.com>
2022-08-10 18:40:12 -0700

    feat: view renderer with variable statements and base types

--- packages/codegen-ui-react/lib/views/react-view-renderer.ts
+++ packages/codegen-ui-react/lib/views/react-view-renderer.ts
@@ -22,12 +22,38 @@ import {
   TableDefinition,
   generateTableDefinition,
   ViewMetadata,
+  handleCodegenErrors,
+  validateViewSchema,
 } from '@aws-amplify/codegen-ui';
-import { JsxElement, JsxFragment, JsxSelfClosingElement } from 'typescript';
-import { ImportCollection } from '../imports';
+import {
+  addSyntheticLeadingComment,
+  BindingElement,
+  EmitHint,
+  factory,
+  FunctionDeclaration,
+  JsxElement,
+  JsxFragment,
+  JsxSelfClosingElement,
+  Modifier,
+  NodeFlags,
+  ScriptKind,
+  Statement,
+  SyntaxKind,
+  TypeAliasDeclaration,
+} from 'typescript';
+import { EOL } from 'os';
+import {
+  buildBaseCollectionVariableStatement,
+  buildPrinter,
+  defaultRenderConfig,
+  getDeclarationFilename,
+  transpile,
+} from '../react-studio-template-renderer-helper';
+import { ImportCollection, ImportSource, ImportValue } from '../imports';
+import { Primitive, PrimitiveTypeParameter } from '../primitive';
+import { getComponentPropName } from '../react-component-render-helper';
 import { ReactOutputManager } from '../react-output-manager';
 import { ReactRenderConfig, scriptKindToFileExtension } from '../react-render-config';
-import { defaultRenderConfig } from '../react-studio-template-renderer-helper';
 import { RequiredKeys } from '../utils/type-utils';
 
 export abstract class ReactViewTemplateRenderer extends StudioTemplateRenderer<
@@ -78,15 +104,286 @@ export abstract class ReactViewTemplateRenderer extends StudioTemplateRenderer<
     };
   }
 
-  renderComponentInternal(): {
-    componentText: string;
-    renderComponentToFilesystem: (outputPath: string) => Promise<void>;
-  } {
-    throw new Error('Method not implemented.');
+  @handleCodegenErrors
+  renderComponentOnly() {
+    const { printer, file } = buildPrinter(this.fileName, this.renderConfig);
+
+    const variableStatements = this.buildVariableStatements();
+    const jsx = this.renderJsx(this.viewComponent);
+    const requiredDataModels = [];
+
+    const imports = this.importCollection.buildImportStatements();
+
+    let importsText = '';
+
+    imports.forEach((importStatement) => {
+      const result = printer.printNode(EmitHint.Unspecified, importStatement, file);
+      importsText += result + EOL;
+    });
+
+    const wrappedFunction = this.renderFunctionWrapper(this.component.name, variableStatements, jsx, false);
+
+    const result = printer.printNode(EmitHint.Unspecified, wrappedFunction, file);
+
+    // do not produce declaration becuase it is not used
+    const { componentText: compText } = transpile(result, { ...this.renderConfig, renderTypeDeclarations: false });
+
+    const { type, model } = this.viewComponent.dataSource;
+    if (type === 'DataStore' && model) {
+      requiredDataModels.push(model);
+      // TODO: require other models if form is handling querying relational models
+    }
+
+    return { compText, importsText, requiredDataModels };
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @handleCodegenErrors
+  renderComponentInternal() {
+    const { printer, file } = buildPrinter(this.fileName, this.renderConfig);
+
+    const variableStatements = this.buildVariableStatements();
+    const jsx = this.renderJsx(this.viewComponent);
+
+    const wrappedFunction = this.renderFunctionWrapper(this.component.name, variableStatements, jsx, true);
+    const propsDeclaration = this.renderBindingPropsType();
+
+    const imports = this.importCollection.buildImportStatements();
+
+    let componentText = `/* eslint-disable */${EOL}`;
+
+    imports.forEach((importStatement) => {
+      const result = printer.printNode(EmitHint.Unspecified, importStatement, file);
+      componentText += result + EOL;
+    });
+
+    componentText += EOL;
+
+    propsDeclaration.forEach((typeNode) => {
+      const propsPrinted = printer.printNode(EmitHint.Unspecified, typeNode, file);
+      componentText += propsPrinted;
+    });
+
+    const result = printer.printNode(EmitHint.Unspecified, wrappedFunction, file);
+    componentText += result;
+
+    const { componentText: transpiledComponentText, declaration } = transpile(componentText, this.renderConfig);
+
+    return {
+      componentText: transpiledComponentText,
+      declaration,
+      renderComponentToFilesystem: async (outputPath: string) => {
+        await this.renderComponentToFilesystem(transpiledComponentText)(this.fileName)(outputPath);
+        if (declaration) {
+          await this.renderComponentToFilesystem(declaration)(getDeclarationFilename(this.fileName))(outputPath);
+        }
+      },
+    };
+  }
+
+  renderFunctionWrapper(
+    componentName: string,
+    variableStatements: Statement[],
+    jsx: JsxElement | JsxFragment | JsxSelfClosingElement,
+    renderExport: boolean,
+  ): FunctionDeclaration {
+    const componentPropType = getComponentPropName(componentName);
+    const jsxStatement = factory.createReturnStatement(
+      factory.createParenthesizedExpression(
+        this.renderConfig.script !== ScriptKind.TSX
+          ? jsx
+          : /* add ts-ignore comment above jsx statement. Generated props are incompatible with amplify-ui props */
+            addSyntheticLeadingComment(
+              factory.createParenthesizedExpression(jsx),
+              SyntaxKind.MultiLineCommentTrivia,
+              ' @ts-ignore: TS2322 ',
+              true,
+            ),
+      ),
+    );
+    const codeBlockContent = variableStatements.concat([jsxStatement]);
+    const modifiers: Modifier[] = renderExport
+      ? [factory.createModifier(SyntaxKind.ExportKeyword), factory.createModifier(SyntaxKind.DefaultKeyword)]
+      : [];
+    const typeParameter = PrimitiveTypeParameter[Primitive[this.viewComponent?.viewConfiguration.type as Primitive]];
+    // only use type parameter reference if one was declared
+    const typeParameterReference = typeParameter && typeParameter.declaration() ? typeParameter.reference() : undefined;
+    return factory.createFunctionDeclaration(
+      undefined,
+      modifiers,
+      undefined,
+      factory.createIdentifier(componentName),
+      typeParameter ? typeParameter.declaration() : undefined,
+      [
+        factory.createParameterDeclaration(
+          undefined,
+          undefined,
+          undefined,
+          'props',
+          undefined,
+          factory.createTypeReferenceNode(componentPropType, typeParameterReference),
+          undefined,
+        ),
+      ],
+      factory.createTypeReferenceNode(
+        factory.createQualifiedName(factory.createIdentifier('React'), factory.createIdentifier('ReactElement')),
+        undefined,
+      ),
+      factory.createBlock(codeBlockContent, true),
+    );
+  }
+
+  buildVariableStatements() {
+    const statements: Statement[] = [];
+    const elements: BindingElement[] = [];
+
+    // props
+    const props = [
+      factory.createBindingElement(undefined, undefined, factory.createIdentifier('items'), undefined),
+      factory.createBindingElement(undefined, undefined, factory.createIdentifier('predicateOverride'), undefined),
+      factory.createBindingElement(undefined, undefined, factory.createIdentifier('formatOverride'), undefined),
+      factory.createBindingElement(undefined, undefined, factory.createIdentifier('highlightOnHover'), undefined),
+      factory.createBindingElement(undefined, undefined, factory.createIdentifier('onRowClick'), undefined),
+      factory.createBindingElement(undefined, undefined, factory.createIdentifier('disableHeaders'), undefined),
+    ];
+    elements.push(...props);
+
+    // get rest of props to pass to top level component
+    elements.push(
+      factory.createBindingElement(
+        factory.createToken(SyntaxKind.DotDotDotToken),
+        undefined,
+        factory.createIdentifier('rest'),
+        undefined,
+      ),
+    );
+
+    // add binding elments to statements
+    statements.push(
+      factory.createVariableStatement(
+        undefined,
+        factory.createVariableDeclarationList(
+          [
+            factory.createVariableDeclaration(
+              factory.createObjectBindingPattern(elements),
+              undefined,
+              undefined,
+              factory.createIdentifier('props'),
+            ),
+          ],
+          NodeFlags.Const,
+        ),
+      ),
+    );
+
+    // add model import for datastore type
+    if (this.component.dataSource.type === 'DataStore') {
+      this.importCollection.addImport(ImportSource.LOCAL_MODELS, this.component.dataSource.type);
+    }
+
+    /*
+    if datastore enabled
+    const myViewDataStore = useDataStoreBinding({
+      model: Model,
+      type: 'Collection'
+    }).items;
+    const items = itemsProp !== undefined ? itemsProp : myViewDataStore;
+
+    if custom enabled
+    const myViewDataStore = [];
+    const items = itemsProp !== undefined ? itemsProp : myViewDataStore;
+    */
+    statements.push(
+      this.buildCollectionBindingCall(),
+      factory.createVariableStatement(
+        undefined,
+        factory.createVariableDeclarationList(
+          [
+            factory.createVariableDeclaration(
+              factory.createIdentifier('items'),
+              undefined,
+              undefined,
+              factory.createConditionalExpression(
+                factory.createBinaryExpression(
+                  factory.createIdentifier('itemsProp'),
+                  factory.createToken(SyntaxKind.ExclamationEqualsEqualsToken),
+                  factory.createIdentifier('undefined'),
+                ),
+                factory.createToken(SyntaxKind.QuestionToken),
+                factory.createIdentifier('itemsProp'),
+                factory.createToken(SyntaxKind.ColonToken),
+                factory.createIdentifier('itemsDataStore'),
+              ),
+            ),
+          ],
+          NodeFlags.Const,
+        ),
+      ),
+    );
+    return statements;
+  }
+
+  private buildCollectionBindingCall() {
+    const { type, model } = this.viewComponent.dataSource;
+    const itemsName = `${this.viewComponent.name}DataStore`;
+    if (type === 'DataStore' && model) {
+      this.importCollection.addMappedImport(ImportValue.USE_DATA_STORE_BINDING);
+      const objectProperties = [
+        factory.createPropertyAssignment(factory.createIdentifier('type'), factory.createStringLiteral('collection')),
+        factory.createPropertyAssignment(factory.createIdentifier('model'), factory.createIdentifier(model)),
+      ];
+
+      const callExp = factory.createCallExpression(factory.createIdentifier('useDataStoreBinding'), undefined, [
+        factory.createObjectLiteralExpression(objectProperties, true),
+      ]);
+      return buildBaseCollectionVariableStatement(factory.createIdentifier(itemsName), callExp);
+    }
+    return factory.createVariableStatement(
+      undefined,
+      factory.createVariableDeclarationList(
+        [
+          factory.createVariableDeclaration(
+            factory.createIdentifier(itemsName),
+            undefined,
+            undefined,
+            factory.createArrayLiteralExpression([], false),
+          ),
+        ],
+        NodeFlags.Const,
+      ),
+    );
+  }
+
+  private renderBindingPropsType(): TypeAliasDeclaration[] {
+    const escapeHatchTypeNode = factory.createTypeLiteralNode([
+      factory.createPropertySignature(
+        undefined,
+        factory.createIdentifier('overrides'),
+        factory.createToken(SyntaxKind.QuestionToken),
+        factory.createUnionTypeNode([
+          factory.createTypeReferenceNode(factory.createIdentifier('EscapeHatchProps'), undefined),
+          factory.createKeywordTypeNode(SyntaxKind.UndefinedKeyword),
+          factory.createLiteralTypeNode(factory.createNull()),
+        ]),
+      ),
+    ]);
+    const formPropType = getComponentPropName(this.component.name);
+
+    this.importCollection.addMappedImport(ImportValue.ESCAPE_HATCH_PROPS);
+
+    return [
+      factory.createTypeAliasDeclaration(
+        undefined,
+        [factory.createModifier(SyntaxKind.ExportKeyword)],
+        factory.createIdentifier(formPropType),
+        undefined,
+        factory.createTypeReferenceNode(factory.createIdentifier('React.PropsWithChildren'), [
+          factory.createIntersectionTypeNode([escapeHatchTypeNode]),
+        ]),
+      ),
+    ];
+  }
+
   validateSchema(component: StudioView): void {
-    // throw new Error('Method not implemented.');
+    validateViewSchema(component);
   }
 }
