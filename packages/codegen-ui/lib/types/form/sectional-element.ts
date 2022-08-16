1e4e29cc2d228775c85f78f9f7f28aa3e3f4f111
Hein Jeong <heinje@amazon.com>
2022-08-16 19:42:03 +0000

    chore: update form sectional element type

--- packages/codegen-ui/lib/types/form/sectional-element.ts
+++ packages/codegen-ui/lib/types/form/sectional-element.ts
@@ -20,7 +20,7 @@ import { StudioFieldPosition } from './position';
  */
 
 export type SectionalElement = {
-  position: StudioFieldPosition;
+  position?: StudioFieldPosition;
   type: string;
 
   level?: number;
