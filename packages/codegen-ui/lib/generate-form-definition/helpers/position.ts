e760a08039e3c7a07b2aef172d784ebb9f4a14f5
Hein Jeong <73264629+hein-j@users.noreply.github.com>
2022-09-07 12:42:20 -0700

    chore: handle bad form element positions (#619)
    Co-authored-by: Hein Jeong <heinje@amazon.com>

--- packages/codegen-ui/lib/generate-form-definition/helpers/position.ts
+++ packages/codegen-ui/lib/generate-form-definition/helpers/position.ts
@@ -13,6 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
  */
+import { InvalidInputError } from '../../errors';
 import { FormDefinition, StudioFieldPosition } from '../../types';
 
 /**
@@ -38,6 +39,13 @@ export function removeAndReturnItemOnward(indices: [number, number], formDefinit
   return row.splice(indices[1], row.length - indices[1]);
 }
 
+// helper that throws if every element traversed but none mapped
+function throwIfUnmappable(originalQueue: unknown[], requeued: unknown[]) {
+  if (originalQueue.length === requeued.length) {
+    throw new InvalidInputError('Unmappable element positions in form');
+  }
+}
+
 /* eslint-disable no-param-reassign */
 
 /**
@@ -51,67 +59,78 @@ export function mapElementMatrix({
   formDefinition: FormDefinition;
   elementQueue: { name: string; position?: StudioFieldPosition; excluded?: boolean }[];
 }): void {
-  const rightOfElementQueue: typeof elementQueue = [];
+  let belowElementQueue: typeof elementQueue = [...elementQueue];
+  let rightOfElementQueue: typeof elementQueue = [];
 
   // map elements with no position; position below; position fixed to first
-  while (elementQueue.length) {
-    const element = elementQueue.shift();
-    if (!element) {
-      break;
-    }
-    if (element.excluded) {
-      const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
+  while (belowElementQueue.length) {
+    const requeued: typeof elementQueue = [];
+    const tempRightOf: typeof elementQueue = [];
 
-      if (previousIndices) {
-        removeFromMatrix(previousIndices, formDefinition);
-      }
-    } else if (element.position && 'rightOf' in element.position && element.position.rightOf) {
-      rightOfElementQueue.push(element);
-    } else if (element.position && 'below' in element.position && element.position.below) {
-      const relationIndices = findIndices(element.position.below, formDefinition.elementMatrix);
-      if (!relationIndices) {
-        elementQueue.push(element);
-      } else {
+    belowElementQueue.forEach((element) => {
+      if (element.excluded) {
+        const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
+
+        if (previousIndices) {
+          removeFromMatrix(previousIndices, formDefinition);
+        }
+      } else if (element.position && 'rightOf' in element.position && element.position.rightOf) {
+        tempRightOf.push(element);
+      } else if (element.position && 'below' in element.position && element.position.below) {
+        const relationIndices = findIndices(element.position.below, formDefinition.elementMatrix);
+        if (!relationIndices) {
+          requeued.push(element);
+        } else {
+          const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
+          if (previousIndices) {
+            removeFromMatrix(previousIndices, formDefinition);
+          }
+          formDefinition.elementMatrix.splice(relationIndices[0] + 1, 0, [element.name]);
+        }
+      } else if (element.position && 'fixed' in element.position && element.position.fixed === 'first') {
         const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
         if (previousIndices) {
           removeFromMatrix(previousIndices, formDefinition);
         }
-        formDefinition.elementMatrix.splice(relationIndices[0] + 1, 0, [element.name]);
+        formDefinition.elementMatrix.unshift([element.name]);
+      } else {
+        const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
+        if (!previousIndices) {
+          formDefinition.elementMatrix.push([element.name]);
+        }
       }
-    } else if (element.position && 'fixed' in element.position && element.position.fixed === 'first') {
-      const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
-      if (previousIndices) {
-        removeFromMatrix(previousIndices, formDefinition);
-      }
-      formDefinition.elementMatrix.unshift([element.name]);
-    } else {
-      const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
-      if (!previousIndices) {
-        formDefinition.elementMatrix.push([element.name]);
-      }
-    }
+    });
+
+    throwIfUnmappable(belowElementQueue, requeued);
+
+    belowElementQueue = requeued;
+    rightOfElementQueue.push(...tempRightOf);
   }
 
   // map elements with rightOf position
   while (rightOfElementQueue.length) {
-    const element = rightOfElementQueue.shift();
-    if (!element) {
-      break;
-    }
-    if (element.position && 'rightOf' in element.position && element.position.rightOf) {
-      const relationIndices = findIndices(element.position.rightOf, formDefinition.elementMatrix);
-      if (!relationIndices) {
-        rightOfElementQueue.push(element);
-      } else {
-        const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
-        if (previousIndices) {
-          const removedItems = removeAndReturnItemOnward(previousIndices, formDefinition);
-          formDefinition.elementMatrix[relationIndices[0]].splice(relationIndices[1] + 1, 0, ...removedItems);
+    const requeued: typeof elementQueue = [];
+
+    rightOfElementQueue.forEach((element) => {
+      if (element.position && 'rightOf' in element.position && element.position.rightOf) {
+        const relationIndices = findIndices(element.position.rightOf, formDefinition.elementMatrix);
+        if (!relationIndices) {
+          requeued.push(element);
         } else {
-          formDefinition.elementMatrix[relationIndices[0]].splice(relationIndices[1] + 1, 0, element.name);
+          const previousIndices = findIndices(element.name, formDefinition.elementMatrix);
+          if (previousIndices) {
+            const removedItems = removeAndReturnItemOnward(previousIndices, formDefinition);
+            formDefinition.elementMatrix[relationIndices[0]].splice(relationIndices[1] + 1, 0, ...removedItems);
+          } else {
+            formDefinition.elementMatrix[relationIndices[0]].splice(relationIndices[1] + 1, 0, element.name);
+          }
         }
       }
-    }
+    });
+
+    throwIfUnmappable(rightOfElementQueue, requeued);
+
+    rightOfElementQueue = requeued;
   }
 
   // filter out empty rows
