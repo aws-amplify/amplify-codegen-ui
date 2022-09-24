8434c0a8303fcda3890b641f3ff7b1fd168c33af
Josue Ruiz <7465495+SwaySway@users.noreply.github.com>
2022-09-23 17:09:12 -0700

    fix: resvered words when creating state variables (#673)
    use a list of reserved words when checking for valid variable names

--- packages/codegen-ui/lib/utils/form-component-metadata.ts
+++ packages/codegen-ui/lib/utils/form-component-metadata.ts
@@ -22,6 +22,7 @@ import {
   FormDefinitionElement,
   FormDefinitionInputElement,
 } from '../types';
+import { reservedWords } from './reserved-words';
 
 export const getFormFieldStateName = (formName: string) => {
   return [formName.charAt(0).toLowerCase() + formName.slice(1), 'Fields'].join('');
@@ -29,10 +30,14 @@ export const getFormFieldStateName = (formName: string) => {
 
 /**
  * @returns true if string contains special characters except for "." , "_" and 0-9
+ * and not a member of the resveredName set
  */
-export const isValidVariableName = (input: string): boolean => /^[a-zA-Z_$][a-zA-Z_$0-9]*$/g.test(input);
+export const isValidVariableName = (input: string, resveredNames?: Set<string>): boolean => {
+  const preCheck = resveredNames ? !resveredNames.has(input) : true;
+  return /^[a-zA-Z_$][a-zA-Z_$0-9]*$/g.test(input) && preCheck;
+};
 
-export const filterFieldName = (input: string): string => input.split('.')[0].replaceAll(/[^a-zA-Z_$]/g, '');
+export const filterFieldName = (input: string): string => input.split('.')[0].replace(/[^a-zA-Z_$]/g, '');
 
 function elementIsInput(element: FormDefinitionElement): element is FormDefinitionInputElement {
   return element.componentType !== 'Text' && element.componentType !== 'Divider' && element.componentType !== 'Heading';
@@ -40,7 +45,7 @@ function elementIsInput(element: FormDefinitionElement): element is FormDefiniti
 
 // create mapping for field name collisions
 export function generateUniqueFieldName(name: string, sanitizedFieldNames: Set<string>) {
-  let sanitizedFieldName = isValidVariableName(name) ? name : filterFieldName(name);
+  let sanitizedFieldName = isValidVariableName(name, sanitizedFieldNames) ? name : filterFieldName(name);
   let count = 1;
   if (sanitizedFieldNames.has(sanitizedFieldName.toLowerCase()) && !name.includes('.')) {
     let prospectiveNewName = sanitizedFieldName + count;
@@ -56,14 +61,12 @@ export function generateUniqueFieldName(name: string, sanitizedFieldNames: Set<s
 
 export const mapFormMetadata = (form: StudioForm, formDefinition: FormDefinition): FormMetadata => {
   const inputElementEntries = Object.entries(formDefinition.elements).filter(([, element]) => elementIsInput(element));
-  const sanitizedFieldNames = new Set<string>();
+  const sanitizedFieldNames = new Set<string>(reservedWords);
   return {
     id: form.id,
     name: form.name,
     formActionType: form.formActionType,
-    layoutConfigs: {
-      ...formDefinition.form.layoutStyle,
-    },
+    layoutConfigs: formDefinition.form.layoutStyle,
     fieldConfigs: inputElementEntries.reduce<Record<string, FieldConfigMetadata>>((configs, [name, config]) => {
       const updatedConfigs = configs;
       const metadata: FieldConfigMetadata = {
@@ -81,7 +84,10 @@ export const mapFormMetadata = (form: StudioForm, formDefinition: FormDefinition
       if ('dataType' in config && config.dataType) {
         metadata.dataType = config.dataType;
       }
-
+      // we add the name of the model as a resvered word
+      if (form.dataType.dataSourceType === 'DataStore') {
+        sanitizedFieldNames.add(form.dataType.dataTypeName);
+      }
       const sanitizedFieldName = generateUniqueFieldName(name, sanitizedFieldNames);
       if (sanitizedFieldName) {
         metadata.sanitizedFieldName = sanitizedFieldName;
---
+++ packages/codegen-ui/lib/utils/reserved-words.ts
@@ -0,0 +1,80 @@
+/*
+  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+/**
+ * uesd so a reserved word is not generated in the component
+ * src: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#reserved_words
+ */
+export const reservedWords = [
+  'await',
+  'break',
+  'case',
+  'catch',
+  'class',
+  'const',
+  'continue',
+  'debugger',
+  'default',
+  'delete',
+  'do',
+  'else',
+  'enum',
+  'export',
+  'extends',
+  'false',
+  'finally',
+  'for',
+  'function',
+  'if',
+  'implements',
+  'import',
+  'in',
+  'instanceof',
+  'interface',
+  'let',
+  'new',
+  'null',
+  'package',
+  'private',
+  'protected',
+  'public',
+  'return',
+  'super',
+  'switch',
+  'static',
+  'this',
+  'throw',
+  'try',
+  'True',
+  'typeof',
+  'var',
+  'void',
+  'while',
+  'with',
+  'yield',
+  'React',
+  // are variables names used wtihin the form
+  'DataStore',
+  'initialValues',
+  'initialData',
+  'onSubmit',
+  'onCancel',
+  'onChange',
+  'onValidate',
+  'fetchByPath',
+  'validateField',
+  'overrides',
+];
