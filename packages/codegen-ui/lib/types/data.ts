5c68348d646223eda43b19a7ccd736cdf7b141ec
swaysway <7465495+SwaySway@users.noreply.github.com>
2022-10-10 23:32:38 +0000

    chore: add dependency on datastore for codegen-ui types

--- packages/codegen-ui/lib/types/data.ts
+++ packages/codegen-ui/lib/types/data.ts
@@ -17,7 +17,6 @@
 // exporting types and scalar functions from aws-amplify
 // as these will be used when loading in dataschema for form generation
 export type { SchemaModel, ModelFields, ModelField, SchemaNonModels, Schema } from '@aws-amplify/datastore';
-export { isGraphQLScalarType } from '@aws-amplify/datastore';
 
 export type SchemaEnums = Record<string, SchemaEnum>;
 export type SchemaEnum = {
