81be5eb73afa01be272623ca5e3e998d8af1a6b9
swaysway <7465495+SwaySway@users.noreply.github.com>
2022-10-11 20:51:50 +0000

    chore: minimize datastore imports to types only

--- packages/test-generator/lib/models/schema.ts
+++ packages/test-generator/lib/models/schema.ts
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
  */
-import { Schema } from '@aws-amplify/codegen-ui';
+import { Schema } from '@aws-amplify/datastore';
 
 export default {
   models: {
