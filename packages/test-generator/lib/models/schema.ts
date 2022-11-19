ce3f0fa9aabbbbeb4021f3169f3e57827eaa177b
Hein Jeong <heinje@amazon.com>
2022-11-19 02:24:17 +0000

    chore: add integ test for hasOne relationship

--- packages/test-generator/lib/models/schema.ts
+++ packages/test-generator/lib/models/schema.ts
@@ -447,6 +447,27 @@ export default {
           isRequired: false,
           attributes: [],
         },
+        HasOneUser: {
+          name: 'HasOneUser',
+          isArray: false,
+          type: {
+            model: 'User',
+          },
+          isRequired: false,
+          attributes: [],
+          association: {
+            connectionType: 'HAS_ONE',
+            associatedWith: 'id',
+            targetName: 'allSupportedFormFieldsHasOneUserId',
+          },
+        },
+        allSupportedFormFieldsHasOneUserId: {
+          name: 'allSupportedFormFieldsHasOneUserId',
+          isArray: false,
+          type: 'ID',
+          isRequired: false,
+          attributes: [],
+        },
         createdAt: {
           name: 'createdAt',
           isArray: false,
