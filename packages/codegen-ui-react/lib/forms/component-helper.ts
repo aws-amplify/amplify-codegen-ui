74e0e91a4f19d95a35145710c5dd35a30546a32c
Scott Young <scoyou@amazon.com>
2022-12-07 13:53:49 -0800

    fix: make TextField a controlled component

--- packages/codegen-ui-react/lib/forms/component-helper.ts
+++ packages/codegen-ui-react/lib/forms/component-helper.ts
@@ -94,6 +94,7 @@ export const renderValueAttribute = ({
 
   const valueIdentifier = currentValueIdentifier || getValueIdentifier(componentName, componentType);
 
+<<<<<<< HEAD
   let renderedFieldName = fieldConfig.sanitizedFieldName || componentName;
   if (fieldConfig.isArray) {
     renderedFieldName = getCurrentValueName(renderedFieldName);
@@ -108,6 +109,9 @@ export const renderValueAttribute = ({
   }
 
   let controlledExpression = factory.createJsxExpression(undefined, fieldNameIdentifier);
+=======
+  let controlledExpression = factory.createJsxExpression(undefined, factory.createIdentifier(componentName));
+>>>>>>> 775abd0 (fix: make TextField a controlled component)
 
   if (dataType && typeof dataType !== 'object' && convertedValueAttributeMap[dataType]) {
     controlledExpression = factory.createJsxExpression(
