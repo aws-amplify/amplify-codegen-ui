323d4c57316cce3121b3b73088053eab693d665f
Hein Jeong <heinje@amazon.com>
2022-12-16 21:02:17 +0000

    chore: tester

--- packages/test-generator/e2e-test-templates/cypress/e2e/e2e-tests.cy.js
+++ packages/test-generator/e2e-test-templates/cypress/e2e/e2e-tests.cy.js
@@ -20,6 +20,8 @@ describe('e2e-tests', () => {
   });
 
   it('renders datastore collection', () => {
-    cy.get('#blogPosts').contains('Working on E2E Tests');
+    cy.contains('error!');
+    // cy.get('#blogPosts').contains('Working on E2E Tests');
+    // cy.get('#actionCardCollection').contains('Yundoo');
   });
 });
