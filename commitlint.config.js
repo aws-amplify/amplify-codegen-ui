45ed04395d315a43d47c21c0dcce0d5439aea922
Chris Woolum <christopher.woolum@gmail.com>
2023-01-19 09:30:56 -0800

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ const {
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
   rules: {
-    'scope-enum': async (context) => [2, 'always', [...(await getPackages(context)), 'release']],
+    'scope-enum': async (context) => [2, 'always', [...(await getPackages(context)), 'release', 'deps']],
     'header-max-length': [2, 'always', 200],
   },
 };
