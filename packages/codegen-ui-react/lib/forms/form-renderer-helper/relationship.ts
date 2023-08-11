a2675904f5449625bba148965433e483507f6111
Roshane Pascual <rotp@amazon.com>
2023-08-11 20:53:00 +0000

    fix: remove belongsto field for update related model expression

--- packages/codegen-ui-react/lib/forms/form-renderer-helper/relationship.ts
+++ packages/codegen-ui-react/lib/forms/form-renderer-helper/relationship.ts
@@ -2042,15 +2042,6 @@ const getUpdateRelatedModelExpression = (
       );
     });
 
-    if (belongsToFieldOnRelatedModel) {
-      statements.push(
-        factory.createPropertyAssignment(
-          factory.createIdentifier(belongsToFieldOnRelatedModel),
-          setToNull ? factory.createNull() : factory.createIdentifier(savedModelName),
-        ),
-      );
-    }
-
     /**
      * API.graphql({
      *    query: updateStudent,
