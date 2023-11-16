7b407f7c58a1c66a0e8834fd0c8986a1127e9e08
David Lopez <lopezbnd@amazon.com>
2023-11-15 17:26:07 -0800

    fix: put the env var in the correct position

--- scripts/integ-templates.sh
+++ scripts/integ-templates.sh
@@ -3,4 +3,4 @@ set -e
 
 lerna run build --scope @aws-amplify/codegen-ui-test-generator
 cp -r packages/test-generator/integration-test-templates/. packages/integration-test
-node DEPENDENCIES='{"aws-amplify": "5.0.0"}' packages/test-generator/dist/generators/GenerateTestApp.js
+DEPENDENCIES='{"aws-amplify": "5.0.0"}' node packages/test-generator/dist/generators/GenerateTestApp.js
