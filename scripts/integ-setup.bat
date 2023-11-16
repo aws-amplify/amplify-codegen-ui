58a159bc553b5563983c704ac4f3aead45d615a3
David Lopez <lopezbnd@amazon.com>
2023-11-16 11:47:54 -0800

    fix: use datastore v4 for windows integ setup

--- scripts/integ-setup.bat
+++ scripts/integ-setup.bat
@@ -14,7 +14,7 @@ call npm run integ:templates
 call lerna bootstrap
 call lerna add --scope integration-test aws-amplify@^5.0.0
 call lerna add --scope integration-test @aws-amplify/ui-react@^5.0.0
-call lerna add --scope integration-test @aws-amplify/datastore@^5.0.0
+call lerna add --scope integration-test @aws-amplify/datastore@^4.0.0
 call lerna add --scope integration-test @aws-amplify/codegen-ui
 call lerna add --scope integration-test @aws-amplify/codegen-ui-react
 call lerna add --scope integration-test @aws-amplify/codegen-ui-test-generator
