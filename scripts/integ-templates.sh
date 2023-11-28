eb2ad437ce49d49966ffc0c0cf35c796d9c25252
David Lopez <lopezbnd@amazon.com>
2023-11-28 13:32:52 -0800

    fix: rename v5 test template

--- scripts/integ-templates.sh
+++ scripts/integ-templates.sh
@@ -2,5 +2,5 @@
 set -e
 
 lerna run build --scope @aws-amplify/codegen-ui-test-generator
-cp -r packages/test-generator/integration-test-templates-v5/. packages/integration-test
+cp -r packages/test-generator/integration-test-templates/. packages/integration-test
 DEPENDENCIES='{"aws-amplify": "5.0.0"}' node packages/test-generator/dist/generators/GenerateTestApp.js
